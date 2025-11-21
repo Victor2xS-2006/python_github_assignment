1d3f3e3e2231f778020b3b1b7225ec6bced7c476
victorsantanasantiago <vsantanasantiago@usf.edu>
2025-11-20 19:19:09 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ hours = input("About how many hours did you spend doing school work today?")
 
 hours = float(hours)
 weekly_hours = hours * 7
+
+print(f"You are on track to spend {weekly_hours} hours doing school work this week.")
